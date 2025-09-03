adcdac7a7145bad9b3142f55f0493912a9203826
mayank-gupta201 <mayankgoyal3005@gmail.com>
2025-09-03 20:26:38 +0530

    basic structure of website

---
+++ src/pages/Index.tsx
@@ -0,0 +1,17 @@
+
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <Hero />
+      <About />
+      <Projects />
+      <DSA />
+      <Certificates />
+      <Contact />
+    </div>
+  );
+};
+
+export default Index;
